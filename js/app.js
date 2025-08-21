f4288d0239647fdd337245814af0a17c8423414a
trong_khuong <nguyen_trong_khuong@allgrow-labo.jp>
2025-08-21 16:26:12 +0700

    fix header change color

--- js/app.js
+++ js/app.js
@@ -9,24 +9,68 @@ window.addEventListener("DOMContentLoaded", (event) => {
 // Header
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector("header");
-  const creative_design = document.querySelector(".js-sec-creative-design");
+  const sec_creative_design = document.querySelector(".js-sec-creative-design");
+  const sec_creative_design_offset_height = sec_creative_design.offsetHeight;
+  // const sec_works = document.querySelector(".js-sec-works");
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        header.classList.add('show');
-      } else {
-        header.classList.remove('show');
-      }
-    });
-  }, {
-    root: null,
-    threshold: 0,
-    rootMargin: "0px 0px -99% 0px"
+  window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
+
+    // Show header when going to creative design section
+    if (scrollY >= sec_creative_design_offset_height) {
+      header.classList.add("is-show");
+    } else {
+      header.classList.remove("is-show");
+    }
+
+    // Change header color when in sec-works area
+    // if (sec_works) {
+    //   const rect = sec_works.getBoundingClientRect();
+
+    //   // Condition: element is still visible in viewport
+    //   const isInView =
+    //     rect.top <= 0 && // touch or surpass the top
+    //     rect.bottom > 0; // still in viewport
+
+    //   if (isInView) {
+    //     header.classList.add("is-change-color");
+    //   } else {
+    //     header.classList.remove("is-change-color");
+    //   }
+    // }
+
   });
-
-  observer.observe(creative_design);
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  const sections = document.querySelectorAll(".js-sec-works"); // (.js-sec-works, .js-sec-test)
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          header.classList.add("is-change-color");
+        } else {
+          header.classList.remove("is-change-color");
+        }
+      });
+    },
+    {
+      root: null, // viewport
+      threshold: 0, // trigger as soon as the top edge enters the viewport
+      rootMargin: "0px 0px -100% 0px", 
+      /*
+       * rootMargin -100% below to ensure class only stays in .js-sec-works area.
+       * When the element slides out of the bottom viewport it will remove the class.
+      */
+    }
+  );
+
+  sections.forEach((section) => observer.observe(section));
+});
+
 
 // Works
 document.addEventListener("DOMContentLoaded", () => {
